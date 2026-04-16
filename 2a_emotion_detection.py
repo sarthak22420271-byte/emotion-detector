c4312725d98f5dc3276d1b36d58bf42ff3f0c5e5
sarthak22420271-byte <sarthak.22420271@viit.ac.in>
2026-04-16 14:00:09 +0530

    Update and rename emotion_detection.py to 2a_emotion_detection.py

--- 2a_emotion_detection.py
+++ 2a_emotion_detection.py
@@ -1,6 +1,11 @@
 import requests
 
 def emotion_detector(text_to_analyze):
+    """
+    This function sends text to Watson NLP Emotion API
+    and returns the detected emotions.
+    """
+
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     
     headers = {
@@ -15,6 +20,7 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=json_data, headers=headers)
 
+    # Return raw JSON if successful
     if response.status_code == 200:
         return response.json()
     else:
